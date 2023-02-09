68cdc2d5412d0828f5fc868605a44a95ec2a2e2e
KawikaSuzuki <suzukik25@up.edu>
2023-02-08 19:53:14 -0800

    2nd unit test fix

--- hw3/tests/sum.test.js
+++ hw3/tests/sum.test.js
@@ -11,5 +11,5 @@ var fs = require('fs'); test('test selectEvent', () => {
     //put the HTML into a testing DOM and do a sanity check 
     document.body.innerHTML = html;
     const $ = require('jquery'); 
-    expect($('h1').html()).toBe("Cheesecake Order Form");
+    expect($('h1').html()).toBe(" Cheesecake Order Form ");
 })
